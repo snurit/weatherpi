29688e0c97cde3101258a0644ae1609d0a594bfa
Sylvain NURIT <sylvain.nurit@gmail.com>
2019-05-03 21:00:02 +0200

    added I2C address for BME680

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 import bme680
 import time
 
-sensor = bme680.BME680()
+try
+    sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+except
+    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
 
 sensor.set_humidity_oversample(bme680.OS_2X)
 sensor.set_pressure_oversample(bme680.OS_4X)
