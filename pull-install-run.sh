7fac9bbd946b779969b96613086f0c3958ff3c22
Sylvain NURIT <sylvain.nurit@gmail.com>
2019-05-03 17:29:24 +0200

    added bash script

---
+++ pull-install-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Pull the latest version from GIT repository
+git pull github master
+
+# Install PIP packages and requirements
+pip install -r requirements.txt
+
+# Run the python script
+if [$# -gt 0]
+    python $1
+fi
